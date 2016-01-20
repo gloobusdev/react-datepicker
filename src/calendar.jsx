89181639a75de715ffd8d1644cd61c5c77f43db4
Wout Mertens <Wout.Mertens@gmail.com>
2016-01-20 11:52:44 +0100

    Remove old code

--- src/calendar.jsx
+++ src/calendar.jsx
@@ -76,9 +76,6 @@ var Calendar = React.createClass({
   },
 
   initializeMomentLocale() {
-    var weekdays = this.props.weekdays.slice(0);
-    weekdays = weekdays.concat(weekdays.splice(0, this.props.weekStart));
-
     this.props.moment.locale(this.props.locale);
 
     this.setState({
